8e283165153e79d203498df788f6c78a6469724b
jimmy <jimmycheong7@gmail.com>
2017-01-28 17:29:41 +0000

    Removed admin authentication, but kept urls and views

--- Django_Lfit/Django_Lfit/settings.py
+++ Django_Lfit/Django_Lfit/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Django_Lfit', 'static'),
 )
 
-LOGIN_REDIRECT_URL = "/account/home"
+#LOGIN_REDIRECT_URL = "/account/home"
 
 
 
--- Django_Lfit/Lfit/views.py
+++ Django_Lfit/Lfit/views.py
@@ -7,23 +7,23 @@ from django.contrib.auth.decorators import login_required
 def login_redirect(request):
 	return redirect('/account/login')
 
-@login_required 
+#@login_required 
 def index(request): 
 	return render(request, 'Lfit/index.html')
 
-@login_required 
+#@login_required 
 def clients(request): 
 	return render(request, 'Lfit/clients.html')
 
-@login_required 
+#@login_required 
 def calendar(request): 
 	return render(request, 'Lfit/calendar.html')
 
-@login_required 
+#@login_required 
 def training(request): 
 	return render(request, 'Lfit/training.html')
 
-@login_required 
+#@login_required 
 def research(request): 
 	return render(request, 'Lfit/research.html')
 
